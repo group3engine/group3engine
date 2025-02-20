290d25b4f543f4c7db43e85192114501d821a0f2
Shahb <Shahb@DESKTOP-G04IECH>
2025-02-20 21:04:42 +0000

    * Light view frustum changed to better capture scene for shadows

--- src/rendering/Light.hpp
+++ src/rendering/Light.hpp
@@ -20,7 +20,7 @@ namespace vk
 		glm::vec4 position;
 		glm::vec4 colour = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 		glm::mat4 LightSpaceMatrix = glm::mat4(1.0f);
-        float view = -8.0f;//-13.0f; // TODO: this is temp and needs to be removed in the future
+        float view = -10.0f;//-13.0f; // TODO: this is temp and needs to be removed in the future
 		float near = 0.1f;
         float far = 55.0f;
 	};
