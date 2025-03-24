191886adb6ba03e22358a133325f8437b4a8e710
dry-cat <ttwq5782@leeds.ac.uk>
2025-03-24 20:50:36 +0000

    Remove commented out code

--- src/DebugRendererImp.h
+++ src/DebugRendererImp.h
@@ -39,8 +39,6 @@ class DebugRendererImp final : public JPH::DebugRendererSimple {
 
     virtual void DrawTriangle(JPH::RVec3Arg inV1, JPH::RVec3Arg inV2, JPH::RVec3Arg inV3, JPH::ColorArg inColor, ECastShadow inCastShadow) override;
 
-    // virtual void DrawTriangle(JPH::RVec3Arg inV1, JPH::RVec3Arg inV2, JPH::RVec3Arg inV3, JPH::ColorArg inColor, ECastShadow inCastShadow) override;
-
     virtual void DrawText3D(JPH::RVec3Arg inPosition, const std::string_view &inString, JPH::ColorArg inColor, float inHeight) override;
 
     void Draw();
