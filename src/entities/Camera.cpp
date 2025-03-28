2e9de25c7b5075c87d1aa94ed9b830aa24579df3
dry-cat <ttwq5782@leeds.ac.uk>
2025-03-28 16:00:04 +0000

    Change character center of screen offset to be half as aggresive

--- src/entities/Camera.cpp
+++ src/entities/Camera.cpp
@@ -50,7 +50,7 @@ void Camera::UpdateCameraMovement(const Transform &character_transform) {
         }
 
         glm::vec3 third_person_camera_offset =
-            ((-2.f * forward) + (1.0f * m_up) + (0.5f * rightVector)) * zoom_level;
+            ((-2.f * forward) + (1.0f * m_up) + (0.25f * rightVector)) * zoom_level;
 
         RRayCast ray;
         ray.mOrigin = Vec3(character_position.x, character_position.y, character_position.z);
