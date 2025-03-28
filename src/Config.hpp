4cd7e84bc5ee33fe574c97745e28d1dcf9dee74c
dry-cat <ttwq5782@leeds.ac.uk>
2025-03-28 12:05:52 +0000

    Update Config.hpp and Config.cpp
    Move them too

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,12 +1,16 @@
 #ifndef GROUP3ENGINE_CONFIG_HPP
 #define GROUP3ENGINE_CONFIG_HPP
 
+#include <cstddef>
+
 namespace GlobalConfig {
 
 // Global toggles
 #ifdef JPH_DEBUG_RENDERER
 extern bool enablePhysicsDebugRenderer;
 #endif // JPH_DEBUG_RENDERER
+
+constexpr size_t maxPlayers = 2;
 
 } // namespace GlobalConfig
 
