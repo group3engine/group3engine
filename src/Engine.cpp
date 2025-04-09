d399fe0ee9885c574bbc3b4027b6bddc613ac04d
dry-cat <ttwq5782@leeds.ac.uk>
2025-04-09 06:40:41 +0100

    Fix LightManager logic
    We were trying to get index 0 for the directional light but the previous logic
    was returning index 1
    
    Also need to reset light masks between scene loads

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -218,6 +218,7 @@ void Engine::ChangeSceneFR(const std::filesystem::path &scenePath, size_t player
     ImGuiRenderer::RemoveTextures();
 
     mScene->Unload();
+    LightManager::getInstance().Unload();
     mMaterialManager->Destroy();
     mMeshManager->Destroy();
     mTextureManager->Destroy();
