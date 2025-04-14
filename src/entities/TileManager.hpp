196dd26af1c59d35110f5320e3675fe568cd746c
dry-cat <ttwq5782@leeds.ac.uk>
2025-04-14 16:22:46 +0100

    Fix typo in TileManager include guard

--- src/entities/TileManager.hpp
+++ src/entities/TileManager.hpp
@@ -3,7 +3,7 @@
 //
 
 #ifndef GROUP3ENGINE_TILEMANAGER_HPP
-#define GROUP3ENGINE_TILEMANAGER_HPPtrue
+#define GROUP3ENGINE_TILEMANAGER_HPP
 #include "Entity.hpp"
 #include "TileBox.hpp"
 class TileManager : public Entity{
