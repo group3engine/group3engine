45ccfd94d543ce74ed34c345a9926b05c4aa3ec6
dry-cat <ttwq5782@leeds.ac.uk>
2025-04-15 21:45:18 +0100

    Fix TBN frame calculation
    Factor out into Common.glsl

--- src/shaders/Common.glsl
+++ src/shaders/Common.glsl
@@ -9,3 +9,18 @@ float SH20(vec3 v) { return 0.315392 * (3.0 * v.z * v.z - 1.0); }
 float SH21(vec3 v) { return 1.092548 * v.x * v.z; }
 float SH22(vec3 v) { return 0.546274 * (v.x * v.x - v.y * v.y); }
 
+// source: https://www.shadertoy.com/view/3s33zj
+mat3 adjugate( in mat4 m )
+{
+    return mat3(cross(m[1].xyz, m[2].xyz),
+                cross(m[2].xyz, m[0].xyz),
+                cross(m[0].xyz, m[1].xyz));
+}
+
+mat3 tbn(vec3 normal, vec4 tangent, mat4 modelMatrix) {
+    mat3 normalMatrix = adjugate(modelMatrix);
+    vec3 normalW = normalize(normalMatrix * normal);
+    vec3 tangentW = normalize(mat3(modelMatrix) * tangent.xyz);
+    vec3 bitangentW = cross(normalW, tangentW) * tangent.w;
+    return mat3(tangentW, bitangentW, normalW);
+}
