3172aa05d32c89346f2fe92d159a035a6a725e0c
dry-cat <ttwq5782@leeds.ac.uk>
2025-04-16 13:35:30 +0100

    (hopefully) fix controller input on windows

--- src/core/InputData.hpp
+++ src/core/InputData.hpp
@@ -189,6 +189,10 @@ enum class GAMEPAD_BUTTON {
     eDPAD_LEFT         = 14,
     eLAST              = 15
 };
+// NOTE: Manually doing per platform cases instead of looking at
+// gamepad mappings that GLFW provides
+// https://www.glfw.org/docs/latest/input_guide.html#gamepad_mapping
+#ifndef WIN32
 enum class GAMEPAD_AXIS
 {
     eLEFT_X             = 0,    // Left stick X axis
@@ -199,6 +203,17 @@ enum class GAMEPAD_AXIS
     eRIGHT_TRIGGER      = 5,    // Right trigger
     eLAST               = 6
 };
+#else
+{
+    eLEFT_X             = 0,    // Left stick X axis
+    eLEFT_Y             = 1,    // Left stick Y axis
+    eRIGHT_X            = 2,    // Right stick X axis
+    eRIGHT_Y            = 3,    // Right stick Y axis
+    eLEFT_TRIGGER       = 4,    // Left trigger
+    eRIGHT_TRIGGER      = 5,    // Right trigger
+    eLAST               = 6
+};
+#endif
 struct InputData {
     struct {
         std::array<uint8_t, static_cast<uint16_t>(KEY::eLAST)> currentKeyState{};
