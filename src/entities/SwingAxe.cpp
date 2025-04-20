7080b8ee5c7dabeae1417dd83043b346225b2c9d
dry-cat <ttwq5782@leeds.ac.uk>
2025-04-20 23:17:15 +0100

    Change to basic cosine fake pendulum simlulation to avoid numerical drift

--- src/entities/SwingAxe.cpp
+++ src/entities/SwingAxe.cpp
@@ -2,12 +2,15 @@
 
 #include <Jolt/Physics/Body/BodyLockMulti.h>
 
+SwingAxe::SwingAxe() {
+    mType = "swing_axe";
+}
+
 void SwingAxe::InitPhysics() {
     GetRigidBody().Init(PhysicsManager::get(), true);
 
     PhysicsManager::get().RegisterEntity(this, GetRigidBody().mBodyId);
 
-    mAngleStart = M_PI / 2;
     // Control how fast the pendulum swings
     if (mFloatProperties.contains("pendulum_length")) {
         mPendulumLength = mFloatProperties["pendulum_length"];
@@ -15,42 +18,26 @@ void SwingAxe::InitPhysics() {
         SPDLOG_ERROR("Swing axe does not have a pendulum length property.");
         exit(EXIT_FAILURE);
     }
-    // Pendulum mass does not matter in this simulation, mass cancels out
-    mPendulumMass = 1.0f;
-    mInertia = mPendulumMass * mPendulumLength*mPendulumLength;
-    mAngularSpeed = 0.0f;
 
     // Scoped lock
     {
-        JPH::BodyLockWrite lock(mLockInterface, GetRigidBody().mBodyId);
+        JPH::BodyLockRead lock(mLockInterface, GetRigidBody().mBodyId);
 
-        JPH::Body &axeBody = lock.GetBody();
+        const JPH::Body &axeBody = lock.GetBody();
 
         // Swing on axis of swing axe body
         mAxisX = axeBody.GetWorldTransform().GetAxisX().Normalized();
         mAxisY = axeBody.GetWorldTransform().GetAxisY().Normalized();
         mAxisZ = axeBody.GetWorldTransform().GetAxisZ().Normalized();
 
-        JPH::Quat rotation = Quat::sRotation(mAxisX, mAngleStart);
-        JPH::Quat newRotation = rotation * axeBody.GetRotation();
-        GetRigidBody().SetRotationJolt(newRotation.Normalized());
+        initialRotation = axeBody.GetRotation();
     }
 }
 
 void SwingAxe::Update(double deltaTime) {
-    JPH::Vec3 gravity = mPhysicsSystem.GetGravity();
+    mTime += deltaTime;
 
-    JPH::Vec3 pendulumUp;
-    {
-        JPH::BodyLockRead lock(mLockInterface, GetRigidBody().mBodyId);
-        pendulumUp = (lock.GetBody().GetWorldTransform().GetAxisY()).Normalized();
-    }
-
-    JPH::Vec3 torque = pendulumUp.Cross(mPendulumMass * -gravity) * deltaTime;
-
-    mAngularSpeed += torque.GetX() / mInertia * deltaTime;
-
-    JPH::Quat rotation = Quat::sRotation(mAxisX, mAngularSpeed);
-    JPH::Quat newRotation = rotation * GetRigidBody().GetRotation();
+    JPH::Quat rotation = Quat::sRotation(mAxisX, JPH_PI / 2.0f * std::cos(mPendulumLength * mTime));
+    JPH::Quat newRotation = rotation * initialRotation;
     GetRigidBody().SetRotationJolt(newRotation.Normalized());
 }
--- src/entities/SwingAxe.hpp
+++ src/entities/SwingAxe.hpp
@@ -7,26 +7,23 @@
 
 class SwingAxe : public Entity {
   public:
-    void InitPhysics() override;
+    SwingAxe();
 
-    // void Awake() override;
+    void InitPhysics() override;
 
     void Update(double deltaTime) override;
 
     PhysicsSystem &mPhysicsSystem = PhysicsManager::get().mPhysicsSystem;
     const JPH::BodyLockInterface &mLockInterface = mPhysicsSystem.GetBodyLockInterface();
 
+    float mPendulumLength = 0.0f;
+
     Vec3 mAxisX;
     Vec3 mAxisY;
     Vec3 mAxisZ;
 
-    float mAngleStart = 0.0f;
+    JPH::Quat initialRotation;
 
-    float mPendulumLength = 0.0f;
-    float mPendulumMass = 0.0f;
-    float mInertia = 0.0f;
-    float mAngularSpeed = 0.0f;
-
-    std::string mType = "swing_axe";
+    float mTime = 0.0f;
 };
 #endif // GROUP3ENGINE_SWINGAXE_HPP
