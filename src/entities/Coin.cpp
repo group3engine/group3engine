b904ba93bbbf594439114212a9396a35700fd6dc
Zachariah Lee <ed20zwyl@leeds.ac.uk>
2025-05-06 13:24:26 +0100

    coins now save and load in correctly

--- src/entities/Coin.cpp
+++ src/entities/Coin.cpp
@@ -4,10 +4,22 @@
 #include "SignalSystem.hpp"
 #include "Signals.hpp"
 #include "AudioManager.hpp"
+#include "Saving.hpp"
+#include "spdlog/spdlog.h"
+
 
 void Coin::Awake() {
     // set the angular velocity of the platform (only around y axis)
     GetRigidBody().SetAngularVelocity(glm::vec3(0, 1, 0));
+    if(Saving::get().Get<bool>(GetName()+"collected") == true)
+    {
+        GetRigidBody().SetActive(false);
+        SetAsInvisible();
+        mCollectedOnStart = true;
+    }
+
+    // get the start scale
+    startScale = GetLocalTransform().scale;
 }
 
 void Coin::OnCollisionStart(Entity *other)
@@ -15,25 +27,56 @@ void Coin::OnCollisionStart(Entity *other)
     // check that the other entity is a player
     if (other->IsCharacter())
     {
-        if(!mCollected)
+        // if it isnt collected (could be collected or dissapearing)
+        if(mCollected == CoinState::UNCOLLECTED)
         {
             // set it to collected
-            mCollected = true;
-
-            SetAsNotSolid(); // bye bye coin
-            SetAsInvisible();
-
-            // send the signal over to the character to add a coin to them
-            CoinSignal coinSignal = {};
-            coinSignal.transmitter = this; // this transmits it
-            coinSignal.receiver = other; // the character recieves it
-            GetScene()->mSignalSystem.EmitSignal(&coinSignal); // send the signal over
-
-            // play coin collection sound
-            glm::vec3 pos = GetLocalTransform().translation; // get the current postition 
-            AudioManager::get().Play3D("arrow", pos.x, pos.y, pos.z); //arrow is placeholder
+            mCollected = CoinState::DISAPPEARING;
+            Collect();
         }
-
-
     }
+}
+
+void Coin::Update(double deltaTime)
+{
+    if(mCollectedOnStart)
+    {
+        Collect();
+        mCollectedOnStart = false;
+    }
+    if(mCollected == CoinState::DISAPPEARING)
+    {
+        float scaleMultiplier = 1.f;
+        timer += deltaTime;
+        scaleMultiplier = std::max(1.f - (timer / timeToShrink), 0.f);
+        
+        // set the scale of the entity
+        glm::vec3 scale = startScale * scaleMultiplier;
+        Transform transform = GetLocalTransform();
+        transform.scale = scale;
+        SetTransform(transform);
+        // if the scale isn't 1, turn off the rigid body
+        if (scaleMultiplier < 0.f) 
+        {
+            GetRigidBody().SetActive(false);
+            SetAsInvisible();
+            mCollected = CoinState::COLLECTED;
+        }
+    }
+}
+
+void Coin::Collect()
+{
+    // send the signal over to the character to add a coin to them
+    CoinSignal coinSignal = {};
+    coinSignal.transmitter = this; // this transmits it
+    GetScene()->mSignalSystem.EmitSignal(&coinSignal); // send the signal over
+    SPDLOG_INFO("sent signal");
+    // play coin collection sound
+    glm::vec3 pos = GetLocalTransform().translation; // get the current postition 
+    AudioManager::get().Play3D("arrow", pos.x, pos.y, pos.z); //arrow is placeholder
+
+
+    //file saving
+    Saving::get().Save(GetName()+"collected", true);
 }
--- src/entities/Coin.hpp
+++ src/entities/Coin.hpp
@@ -3,6 +3,12 @@
 
 #include "Entity.hpp"
 
+enum class CoinState {
+    COLLECTED,
+    DISAPPEARING,
+    UNCOLLECTED
+};
+
 class Coin : public Entity {
 
     public:
@@ -10,10 +16,18 @@ class Coin : public Entity {
 
         void OnCollisionStart(Entity *other) override;
 
+        void Update(double deltaTime) override;
+
         void SetCoinCounter(int* aCoinCount);
+
+        void Collect();
     
     private:
-        bool mCollected = false;
+        CoinState mCollected = CoinState::UNCOLLECTED;
+        float timeToShrink = 0.3f;
+        glm::vec3 startScale{};
+        float timer = 0.f;
+        bool mCollectedOnStart = false;
 
 };
 #endif // GROUP3ENGINE_IDOL_HPP
