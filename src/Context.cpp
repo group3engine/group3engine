995fc034024f4faebbc1ef850f24751dbdd3db09
dry-cat <ttwq5782@leeds.ac.uk>
2025-05-09 14:36:15 +0100

    Revert changing to VK_PRESENT_MODE_IMMEDIATE_KHR

--- src/Context.cpp
+++ src/Context.cpp
@@ -527,8 +527,6 @@ void Context::CreateSwapchain() {
         presentMode = VK_PRESENT_MODE_FIFO_RELAXED_KHR;
     }
 
-    presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
-
     VkSurfaceCapabilitiesKHR caps = {};
     vkGetPhysicalDeviceSurfaceCapabilitiesKHR(pDevice, surface, &caps);
 
